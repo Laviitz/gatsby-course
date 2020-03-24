8bb218e3f6fb4ee71e0c5360a0605a31f49fff5c
Luan <luan.vieira@zup.com.br>
2020-03-24 20:28:53 -0300

    gatsby image

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from 'gatsby';
+import Avatar from "../Avatar";
 
 const Profile = () => {
   const { site: { siteMetadata: { title, position, description } } } = useStaticQuery(
@@ -18,6 +19,7 @@ const Profile = () => {
   
   return (
     <div className="Profile-wrapper">
+      <Avatar/>
       <h1>{title}</h1>
       <h2>{position}</h2>
       <p>{description}</p>
