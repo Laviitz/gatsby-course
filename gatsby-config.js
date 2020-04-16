29f291e2ba6902a10f481004a80a9801202474c3
Luan <luan.vieira@zup.com.br>
2020-04-15 22:54:02 -0300

    config manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,20 +10,22 @@ module.exports = {
     siteUrl: `https://luanvieira.com.br`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    //needs to be the first one to work with gatsby-remark-images
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: 'uploads',
+        name: `uploads`,
         path: `${__dirname}/static/assets/img`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
+        name: `images`,
+        path: `${__dirname}/static/assets/img`,
       },
     },
     {
@@ -40,23 +42,23 @@ module.exports = {
           {
             resolve: "gatsby-remark-relative-images",
             options: {
-              name: "uploads"
-            }
+              name: "uploads",
+            },
           },
           {
             resolve: "gatsby-remark-images",
-            optons: {
+            options: {
               maxWidth: 960,
-              linkImagesToOriginal: false
+              linkImagesToOriginal: false,
             },
           },
-            `gatsby-remark-lazy-load`,
-            `gatsby-remark-prismjs`,
+          `gatsby-remark-lazy-load`,
+          `gatsby-remark-prismjs`,
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-algolia-search`,
       options: {
@@ -69,21 +71,20 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        name: `John Doe`,
+        short_name: `John Doe`,
+        start_url: `/`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
+        display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-transition-link`
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
-};
+}
